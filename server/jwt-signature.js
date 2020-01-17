fc698a77002a40b98667a87da27173391b5c1a25
Alan Eicker <alaneicker@gmail.com>
2020-01-17 13:29:34 -0600

    user token vaerification

---
+++ server/jwt-signature.js
@@ -0,0 +1,1 @@
+export default 'GIDP8xoEHAHitGKSpUsf';
--- server/routes/user/validate.js
+++ server/routes/user/validate.js
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import * as jwt from 'jsonwebtoken';
+import jwtSignature from '../../jwt-signature';
 
 const router = Router();
 
 router.post('/validate', (req, res) => {
-  res.send({});
+  const token = req.body.token;
+
+  if (token) {
+    jwt.verify(token, jwtSignature, (err, decoded) => {
+      if (!err && decoded !== undefined) {
+        return res.send({ isLoggedIn: true, decoded });
+      }
+    });
+  }
+
+  return res.send({ isLoggedIn: false, decoded: null });
 });
 
 export default router;
