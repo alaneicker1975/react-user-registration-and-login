aaa5fe3f3ccff8b05933610e019b5e61d2501b73
Alan Eicker <alaneicker@gmail.com>
2020-01-17 14:56:01 -0600

    moves token re-verification logic to PrivateRoute component

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,9 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+
+import { verifyUser } from './actions/verify-user';
 import { AppContext } from './App';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { state } = useContext(AppContext);
+  const { state, dispatch } = useContext(AppContext);
+
+  useEffect(() => {
+    // If page is refreshed, re-verify user and set user
+    if (performance.navigation.type == 1) {
+      const token = localStorage.getItem('token');
+
+      verifyUser(token)
+        .then(res => res.json())
+        .then(({ isLoggedIn, user }) => {
+          dispatch({ 
+            type: 'SET_USER_AS_LOGGED_IN',
+            payload: { isLoggedIn },
+          });
+          dispatch({ 
+            type: 'SET_USER',
+            payload: { user },
+          });
+        });
+    }
+  }, []);
 
   return (
     <Route
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,30 +1,9 @@
-import React, { Fragment, useContext, useEffect } from 'react';
-import { verifyUser } from '../actions/verify-user';
+import React, { Fragment, useContext } from 'react';
 import { AppContext } from '../App';
 
 const Dashboard = () => {
   const { state, dispatch } = useContext(AppContext);
 
-  useEffect(() => {
-    // If page is refreshed, re-verify user and set user
-    if (performance.navigation.type == 1) {
-      const token = localStorage.getItem('token');
-
-      verifyUser(token)
-        .then(res => res.json())
-        .then(({ isLoggedIn, user }) => {
-          dispatch({ 
-            type: 'SET_USER_AS_LOGGED_IN',
-            payload: { isLoggedIn },
-          });
-          dispatch({ 
-            type: 'SET_USER',
-            payload: { user },
-          });
-        });
-    }
-  }, []);
-  
   return (
     <Fragment>
       <h1>Dashboard</h1>
