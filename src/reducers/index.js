904a33f59f2f2ed3ad66fd99f1cfcbcb44cc3042
Alan Eicker <alaneicker@gmail.com>
2020-01-17 11:11:05 -0600

    Adds reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,20 @@
 export const initialState = { 
   isLoggedIn: false,
+  user: null,
 };
 
 export const reducer = (state, action) => {
+  const { token, user } = action.payload;
+
   switch (action.type) {
     case 'SET_AUTH_TOKEN':
-      // Logic to set cookie
-      return { ...state, isLoggedIn };
-    case 'SET_AUTH_TOKEN':
-      // Logic to logout user
+      localStorage.setItem('token', token);
+      return { ...state, isLoggedIn: true };
+    case 'UNSET_AUTH_TOKEN':
+      localStorage.removeItem('token');
       return { ...state, isLoggedIn: false };
+    case 'SET_USER':
+        return { ...state, user };
     default:
       return state;
   }
