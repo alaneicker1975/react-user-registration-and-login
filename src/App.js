d0a5d02b1a5cf21a8c8f0b3b8a6fa5cd73a7b484
Alan Eicker <alaneicker@gmail.com>
2020-01-17 09:48:36 -0600

    Adds register route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { createContext, useReducer } from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Register from './components/register';
+
 import { reducer, initialState } from './reducers';
 
 export const AppContext = createContext({});
@@ -11,7 +13,7 @@ const App = () => {
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <Router>
-        <Route path="/" component={Dashboard} exact />
+        <Route path="/" component={Register} exact />
       </Router>
     </AppContext.Provider>
   );
