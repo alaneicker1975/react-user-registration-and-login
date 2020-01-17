e9a086c461c0ff6b6aefb6d34f07031c234f7d72
Alan Eicker <alaneicker@gmail.com>
2020-01-17 09:52:25 -0600

    Adds routes and page components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { createContext, useReducer } from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Login from './components/login';
+import Dashboard from './components/dashboard';
 import Register from './components/register';
 
 import { reducer, initialState } from './reducers';
@@ -13,7 +15,10 @@ const App = () => {
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <Router>
-        <Route path="/" component={Register} exact />
+        <Route path="/" component={Login} exact />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/register" component={Register} />
       </Router>
     </AppContext.Provider>
   );
