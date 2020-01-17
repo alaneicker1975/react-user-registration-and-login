2c829242514a350e57e7da158394e080765ac83a
Alan Eicker <alaneicker@gmail.com>
2020-01-17 11:31:50 -0600

    Adds user login and validate endpoints

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 
+import loginRoute from './routes/user/login';
+import validateRoute from './routes/user/validate';
+
+const baseUrl = '/api/v1';
 const PORT = process.env.PORT || 6060;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('dist'));
+
+app.use(`${baseUrl}/user`, loginRoute);
+app.use(`${baseUrl}/user`, validateRoute);
 
 app.listen(PORT, () => {
   console.log('App listening on:', PORT);
---
+++ server/routes/user/validate.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.post('/validate', (req, res) => {
+  res.send({});
+});
+
+export default router;
