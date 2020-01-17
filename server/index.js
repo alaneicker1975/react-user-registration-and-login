5e6e6f0f9facd612af3d049bf07b7d7066cf21cf
Alan Eicker <alaneicker@gmail.com>
2020-01-17 09:59:45 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
 import './dotenv';
-
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 
 const PORT = process.env.PORT || 6060;
-const ENV = process.env.NODE_ENV;
-
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
   console.log('App listening on:', PORT);
