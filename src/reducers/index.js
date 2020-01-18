470f2857b941a445115dc1ab784bc152e95a5f83
Alan Eicker <alaneicker@gmail.com>
2020-01-18 08:19:18 -0600

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,7 +10,6 @@ export const reducer = (state, action) => {
     case 'SET_USER_AS_LOGGED_IN':
       return { ...state, isLoggedIn: true };
     case 'SET_USER_AS_LOGGED_OUT':
-      localStorage.removeItem('token');
       return { ...state, isLoggedIn: false };
     case 'SET_USER':
       return { ...state, username: payload.username };
