281d178061d5238ae56c87911fee29cb0921c9c3
Alan Eicker <alaneicker@gmail.com>
2020-01-18 16:16:39 -0600

    refactoring verifyUserToken logic

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import { verifyUserToken } from './actions/verify-user';
 import { AppContext } from './App';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
@@ -13,15 +12,23 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
       const token = localStorage.getItem('token');
       
       if (token) {
-        verifyUserToken(token)
-          .then(res => res.json())
-          .then(({ isValid }) => {
-            if (!isValid) {
-              dispatch({ type: 'SET_USER_AS_LOGGED_OUT' });
-            } else {
-              dispatch({ type: 'SET_USER_AS_LOGGED_IN' });
-            }
-          });
+        fetch('http://localhost:6060/api/v1/user/verify', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ token }),
+        })
+        .then(res => res.json())
+        .then(({ isValid }) => {
+          if (!isValid) {
+            dispatch({ type: 'SET_USER_AS_LOGGED_OUT' });
+          } else {
+            dispatch({ type: 'SET_USER_AS_LOGGED_IN' });
+          }
+        });
+
         return;
       }
 
