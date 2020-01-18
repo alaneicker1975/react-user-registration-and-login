7bb53245e8a8d849086f8028212c5306dcd64647
Alan Eicker <alaneicker@gmail.com>
2020-01-18 08:18:51 -0600

    adds SET_USER reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,19 @@
 export const initialState = { 
   isLoggedIn: true,
+  username: '',
 };
 
 export const reducer = (state, action) => {
+  const { payload } = action;
+
   switch (action.type) {
     case 'SET_USER_AS_LOGGED_IN':
       return { ...state, isLoggedIn: true };
     case 'SET_USER_AS_LOGGED_OUT':
       localStorage.removeItem('token');
       return { ...state, isLoggedIn: false };
+    case 'SET_USER':
+      return { ...state, username: payload.username };
     default:
       return state;
   }
