29653915331593ca858b8c1d7167792e1aa28eb9
Alan Eicker <alaneicker@gmail.com>
2020-01-18 15:33:03 -0600

    login logic

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
 
 const LoginForm = (props) => {
-  const { onLoginSuccess } = props;
-  
+  const { onValidationSuccess, loginError } = props;
+
   return (
     <form>
       Login form
     </form>
   );
 }
+
+LoginForm.propTypes = {
+  onValidationSuccess: PropTypes.func,
+  loginError: PropTypes.bool,
+};
+
+LoginForm.defaultProps = {
+  onValidationSuccess: () => {},
+  loginError: false,
+};
 
 export default LoginForm;
--- src/pages/login.js
+++ src/pages/login.js
@@ -5,11 +5,28 @@ import { AppContext } from '../App';
 
 const Login = () => {
   const { dispatch } = useContext(AppContext);
-  const [redirect, setRedirect] = useState(false);
+  const [ redirect, setRedirect ] = useState(false);
+  const [ loginError, setLoginError ] = useState(false);
   
-  const handleLoginSuccess = () => {
-    dispatch({ type: 'SET_USER_AS_LOGGED_IN' });
-    setRedirect(true);
+  const submitFormData = async (formData) => {
+    try {
+      const { token } = fetch('http://localhost:6060/api/v1/user/authenticate', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ formData }),
+      });
+
+      if (token) {
+        localStorage.setItem('token', token);
+        dispatch({ type: 'SET_USER_AS_LOGGED_IN' });
+        setRedirect(true);
+      }
+    } catch (err) {
+      setLoginError(true);
+    }
   }
 
   return redirect
@@ -17,7 +34,10 @@ const Login = () => {
     : (
       <Fragment>
         <h1>Login</h1>
-        <LoginForm onLoginSuccess={handleLoginSuccess} />
+        <LoginForm 
+          onValidationSuccess={submitFormData} 
+          loginError={loginError}
+        />
       </Fragment>
     );
 };
