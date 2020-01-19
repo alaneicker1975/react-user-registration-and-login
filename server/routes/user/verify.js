6ae888dabd2ab7651986baa63dc7c486ba0a1ef6
Alan Eicker <alaneicker@gmail.com>
2020-01-19 08:51:55 -0600

    Update verify.js

--- server/routes/user/verify.js
+++ server/routes/user/verify.js
@@ -6,12 +6,12 @@ const router = Router();
 
 router.post('/verify', (req, res) => {
   const trustedOrigin = 'http://localhost:1234'; // TODO: Store this value as environment variable
-  const { headers: { origin, referer } } = req;
+  const { body: { token }, headers: { origin, referer } } = req;
   
   if (origin !== trustedOrigin || !referer.includes(trustedOrigin)) {
     res.send({ isValid: false });
   }
-
+  
   jwt.verify(token, jwtSignature, (err, decoded) => {
     if (err || decoded === undefined) {
       res.send({ isValid: false });
