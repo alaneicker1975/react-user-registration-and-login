a06b3a67232079b50e8b43a0ee9b26c39f63b354
Alan Eicker <alaneicker@gmail.com>
2020-01-25 21:46:40 -0600

    UserList tests

--- src/components/user-list/user-list.test.js
+++ src/components/user-list/user-list.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import React from 'react';
+import { act } from 'react-test-renderer';
 import { mount } from 'enzyme';
 import UserList from '.';
 import { AppContext } from '../../App';
@@ -8,14 +9,14 @@ describe('UserList', () => {
 
   let component;
 
-  beforeEach(() => {
-    const context = {
-      state: {
-        users: [{ username: 'testuser', isAdmin: 0 }]
-      },
-      dispatch() {},
-    };
+  const context = {
+    state: {
+      users: [{ username: 'testuser', isAdmin: 0 }]
+    },
+    dispatch() {},
+  };
 
+  beforeEach(() => {
     component = mount(
       <AppContext.Provider value={context}>
         <UserList />
@@ -29,6 +30,34 @@ describe('UserList', () => {
 
   it('Should render a list', () => {
     expect(component.find('.user-list').children().length).toBe(1);
+  });
+
+  it('Should fetch users and render to a list', async done => {
+    const mockSuccessResponse = [
+      { username: 'bobsmith', isAdmin: 0 },
+      { username: 'paulblack', isAdmin: 1 },
+    ];
+
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse); 
+    const mockFetchPromise = Promise.resolve({ 
+      json: () => mockJsonPromise,
+    });
+
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    await act(async () => {
+      mount(
+        <AppContext.Provider value={context}>
+          <UserList />
+        </AppContext.Provider>
+      );
+    });
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('http://localhost:6060/api/v1/users');
+
+    global.fetch.mockClear();
+    done();
   });
 
 });
