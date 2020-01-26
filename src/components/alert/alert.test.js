62f6c88f50779d4c49f7ca885cdbb70c9c510bb6
Alan Eicker <alaneicker@gmail.com>
2020-01-25 20:29:48 -0600

    Alert unit tests

--- src/components/alert/alert.test.js
+++ src/components/alert/alert.test.js
@@ -7,7 +7,25 @@ describe('Alert', () => {
 
   it('Should render without errors', () => {
     const component = shallow(<Alert />);
+
     expect(component.length).toBe(1);
   });
 
+  it('Should render message text', () => {
+    const component = shallow(<Alert text="An error has occurred" />);
+    
+    expect(component.find('.alert').text()).toBe('An error has occurred');
+  });
+
+  it('Should add .alert--error class', () => {
+    const component = shallow(<Alert type="error" />);
+    
+    expect(component.find('.alert').hasClass('alert--error')).toBe(true);
+  });
+
+  it('Should add .alert--right class', () => {
+    const component = shallow(<Alert align="right" />);
+    
+    expect(component.find('.alert').hasClass('alert--right')).toBe(true);
+  });
 });
